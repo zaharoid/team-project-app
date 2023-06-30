e3dfaa8d14c97530d5d3a80d2c8bcad124b139f0
zaharoid <gololobov.zahar23@gmail.com>
2023-06-30 20:58:16 +0200

    add root

--- src/js/books-container.js
+++ src/js/books-container.js
@@ -1,18 +1,19 @@
-import { fetchTopBooks } from './books-api'
+import { fetchTopBooks } from './books-api';
 const refs = {
-  categorieslist: document.querySelector('.categories-list'),
-}
-fetchTopBooks().then(console.log);
+  categoriesList: document.querySelector('.categories-list'),
+};
 fetchTopBooks().then(renderAllCards);
 
 async function renderAllCards(data) {
   data.forEach(category => {
     let markup_base = `<li class="category">
     <h3>${category.books[0].list_name}</h3>
-    <ul class="bookslist">`
+    <ul class="bookslist">`;
     let markup = ``;
     category.books.map(book => {
-    markup = markup + `
+      markup =
+        markup +
+        `
       <li class="book-card">
         <div class="thumb">
           <img class="book-cover" src="${book.book_image}" alt="${book.title}" loading="lazy" />
@@ -20,9 +21,11 @@ async function renderAllCards(data) {
         <h4 class="book-title">${book.title}</h4>
         <p>${book.author}</p>
       </li>`;
-    
     });
-    markup_base = markup_base + markup + `</ul><div class="button-container"><button>See more</button></div></li>`;
-    refs.categorieslist.insertAdjacentHTML('beforeend', markup_base);
+    markup_base =
+      markup_base +
+      markup +
+      `</ul><div class="button-container"><button>See more</button></div></li>`;
+    refs.categoriesList.insertAdjacentHTML('beforeend', markup_base);
   });
 }
