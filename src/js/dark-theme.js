0bfdbd5cbe8a1bb8579fee010793c2c30686bc29
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-07-02 17:17:57 +0100

    new changes in adaptation

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -4,51 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelector('.switch').addEventListener('click', event => {
     const theme = switchInput.checked ? 'dark' : '';
-    console.log('theme', theme);
-
     if (theme === 'dark') {
       localStorage.setItem('theme', 'dark');
-      localStorage.setItem('p', 'color-grey'); //
     } else {
       localStorage.removeItem('theme');
-      localStorage.removeItem('p', 'color-grey'); //
     }
 
     addDarkClassToHTML();
-    updateSliderClass();
   });
 
   function addDarkClassToHTML() {
     try {
       if (localStorage.getItem('theme') === 'dark') {
-        document.querySelector('html').classList.add('dark');
-
-        document
-          .querySelector('.categories-list')
-          .classList.add('categories-text-color-dark');
-
-        const paragraphs = document.querySelectorAll('p');
-        console.log('get all p', paragraphs);
-
-        //add class 'color-grey' for all <p></p>
-        paragraphs.forEach(paragraph => {
-          paragraph.classList.add('color-grey');
-        });
+        document.querySelector('body').classList.toggle('dark-theme');
       } else {
-        document.querySelector('html').classList.remove('dark');
-        document
-          .querySelector('.categories-list')
-          .classList.remove('categories-text-color-dark');
-
-        document
-          .querySelector('.categories-list li')
-          .classList.remove('color-grey');
-
-        const paragraphs = document.querySelectorAll('p');
-        console.log('all p', paragraphs);
-        paragraphs.forEach(paragraph => {
-          paragraph.classList.remove('color-grey');
-        });
+        document.querySelector('body').classList.remove('dark-theme');
       }
     } catch (error) {
       console.log(error);
@@ -58,9 +28,9 @@ document.addEventListener('DOMContentLoaded', () => {
   function updateSliderClass() {
     const slider = document.querySelector('.slider');
     if (localStorage.getItem('theme') === 'dark') {
-      slider.classList.add('dark');
+      slider.classList.add('dark-theme');
     } else {
-      slider.classList.remove('dark');
+      slider.classList.remove('dark-theme');
     }
   }
 
