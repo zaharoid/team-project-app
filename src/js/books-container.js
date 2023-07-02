039ac975fb364d9b7829ee1c091dcc30485877d6
vldslvD <dobritsavlad@gmail.com>
2023-07-02 19:04:06 +0200

    styles to main, update list

--- src/js/books-container.js
+++ src/js/books-container.js
@@ -1,14 +1,13 @@
 import { fetchTopBooks } from './books-api';
 const refs = {
-  categoriesList: document.querySelector('.categories-list'),
+  categoriesList: document.querySelector('.books-in-categories-list'),
 };
 fetchTopBooks().then(renderAllCards);
-
 async function renderAllCards(data) {
   let booksArr = [];
 
   data.forEach(category => {
-    let markup_base = `<li class="category">
+    let markup_base = `<li class="category-item">
     <h3>${category.books[0].list_name}</h3>
     <ul class="bookslist">`;
     let markup = ``;
@@ -29,9 +28,18 @@ async function renderAllCards(data) {
     markup_base =
       markup_base +
       markup +
-      `</ul><div class="button-container"><button>See more</button></div></li>`;
+      `</ul><div class="button-container" ><button class="books-by-category" data-category-name="${category.books[0].list_name}">See more</button></div></li>`;
     refs.categoriesList.insertAdjacentHTML('beforeend', markup_base);
   });
 
   localStorage.setItem('books', JSON.stringify(booksArr));
+}
+
+refs.categoriesList.addEventListener('click', onMoreBtnClick);
+
+function onMoreBtnClick(e) {
+  if (e.target.nodeName !== "BUTTON") {
+    return;
+  }
+  //console.log(e.target.getAttribute('data-category-name'))
 }
