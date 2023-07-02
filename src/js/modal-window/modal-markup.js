5133df7058f8165b6fd4e69c307b8c2dc6cf1cee
KRVitalik <karmelitavitalik@gmail.com>
2023-07-02 17:08:06 +0300

    comit 2

--- src/js/modal-window/modal-markup.js
+++ src/js/modal-window/modal-markup.js
@@ -1,34 +1,60 @@
 const icon = {
-    closeBtn: 'href="../images/icons.svg#icon-logo"',
-    amazon: 'href="../images/image.svg#pattern0"',
+    amazon: '../../images/image.svg#pattern0',
     Bookshop: '',
-    "Apple Books": ''
+    "Apple Books": '',
+    closeIcon:"../../images/icons.svg#icon-x-close"
 }
 
+    //     <svg class="modal-svg" width="24px" height="24px">
+    // <use href='../images/support-ua/action-against-hunger.png'></use>
+    // </svg >
+
+        
 export function createMarkup(data) {
     return `
     <div class="modal-main">
-    <svg class="modal-svg" width="24px" height="24px">
-    <use href=${icon.amazon}></use>
-    </svg>
-    <img class="modal-cover" src="${data.book_image
-    }" alt="${data.title}" />
+    <button class="modal_btn_close" type="button">
+    <svg class="modal-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="3.3333" d="M24 8l-16 16M8 8l16 16"></path>
+        </svg>
+    </button>
+    <div class="modal_book_info">
+    <img class="modal-cover"
+    src="${data.book_image}" 
+    alt="${data.title}" />
+    <div class="modal_text_info">
     <h2 class="modal-title">${data.title}</h2>
     <p class="modal-author">${data.author}</p>
     <p class="modal-description">${data.description}</p>
-    <ul class="modal-books-shops">${createBookSeller(data)}</ul>
+    <ul class="modal-books-shops">${createBookSeller(data)}</ul></div></div>
     <button class="modal-add-btn" type="button">ADD TO SHOPPING LIST</button>
     </div>
     `
 }
-
-function createBookSeller({buy_links}) {
+function createBookSeller({ buy_links }) {
     return buy_links.map(arr => {
-        if (arr.name === "Amazon" && "Bookshop" && "Apple Books") {
-            console.log(arr.name)
+        console.log('arr', arr)
+        if (arr.name === "Amazon") {
             return `<li>
-        <a href="${arr.url}" target="_blank"><svg class="modal-svg" width="24px" height="24px">
-        <use href="./images/icons.svg#icon-arms-icon"></use>
+        <a href=${arr.url} target="_blank">
+        <svg class="modal-svg" width="24px" height="24px">
+        <use href=${icon.closeIcon}></use>
+        </svg></a>
+        </li>
+        `}
+        if (arr.name === "Bookshop") {
+            return `<li>
+        <a href="${arr.url}" target="_blank">
+        <svg class="modal-svg" width="24px" height="24px">
+        <use href='${icon.closeIcon}'></use>
+        </svg></a>
+        </li>
+        `}
+        if (arr.name === "Apple Books") {
+            return `<li>
+        <a href="${arr.url}" target="_blank">
+        <svg class="modal-svg" width="24px" height="24px">
+        <use href="../../images/icons.svg#icon-x-close"></use>
         </svg></a>
         </li>
         `}
--- src/js/modal-window/modal.js
+++ src/js/modal-window/modal.js
@@ -1,21 +1,64 @@
 import { fetchBookID } from '../books-api'
 import { createMarkup } from "./modal-markup";
 
-const modalBody = document.querySelector("body")
+let modalBody = document.querySelector("body")
+let modalOpenWindow = document.querySelector(".modal_window")
 
-modalBody.addEventListener('click', (currentBook))
 
-fetchBookID("643282b1e85766588626a0c2").then(data => {
-    console.log('data', data)
-    createMarkup(data)
-    modalBody.insertAdjacentHTML('beforeend', createMarkup(data))
-})
+modalBody.addEventListener('click', currentBook)
 
-function currentBook(e) {
-        // e.preventDefault();
-    
-    // if (e.target.parentElement !== 'li.book-card') {
-    //     return
-    // }
-    console.log(e)
+function eventListeners() {
+    const buttonClose = document.querySelector(".modal_btn_close")
+    buttonClose.addEventListener("click", closeModal)
+    modalBody.addEventListener('click', onOverlayCloseModal)
+    modalBody.addEventListener('keydown', onEscCloseModal)
+    modalBody.addEventListener("scroll", (e) => { e.preventDefault() })
 }
+
+function currentBook() {    
+    const book = document.querySelector('[data-id]');
+    console.dir(book)
+    const bookCards = document.querySelectorAll('[data-id]');
+    console.log(bookCards)
+    bookCards.forEach(card => {
+        card.addEventListener('click', () => {
+            const bookId = card.dataset.id;
+            fetchBookID(bookId).then(data => {
+                console.log('data', data)
+                modalOpenWindow.innerHTML = createMarkup(data)
+                modalOpenWindow.classList.add("overlay")
+                blockScroll()
+                eventListeners()
+            }).catch((error) => {
+      console.log('Error:', error);
+    });
+        });
+    });
+}
+
+function blockScroll() {
+    modalBody.classList.add("no_scroll")
+}
+
+function closeModal() {
+    modalBody.classList.remove("no_scroll")
+    modalOpenWindow.classList.remove("overlay")
+    modalOpenWindow.innerHTML = ""
+    modalBody.removeEventListener('click', onOverlayCloseModal)
+    modalBody.removeEventListener('keydown', onEscCloseModal)
+}
+
+function onOverlayCloseModal(e) {
+    if (e.target !== modalOpenWindow) {
+        return
+    }
+    closeModal()
+}
+
+function onEscCloseModal(e) {
+    if (e.code !== "Escape") {
+        return
+    }
+    closeModal()
+}
+
