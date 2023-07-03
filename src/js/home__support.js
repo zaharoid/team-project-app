f93617a3cf2dc7ed553f26cf4e1b099c25cb90f4
Serhii Balaba <122040264+BalabaSerhii@users.noreply.github.com>
2023-07-03 20:06:00 +0200

    hover s (#37)

--- src/js/home__support.js
+++ src/js/home__support.js
@@ -88,7 +88,7 @@ function createImageCardMarkup(supportArray) {
       return `<li class="support-link js-support">
         <a class="js-target" href="${url}" target="_blank">
           <span class="support-number">${num}
-          <img class="support-img" srcset="${img} 1x, ${img2x} 2x"
+          <img class="support__img" srcset="${img} 1x, ${img2x} 2x"
           src="${img}" alt="${title}"/></span>
         </a>
       </li>`;
