761c4ebc8065890d03dfe55e255ca7f65bf962c2
Anatolii Tokariev <anatoliytockarev2709@gmail.com>
2023-07-03 23:05:54 +0200

    js-burger

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,10 @@
-function toggleMenuIcon() {
-  const menuIconUse = document.getElementById('menu-icon-use');
-  const currentIcon = menuIconUse.getAttribute('href');
+const menuOpen = document.querySelector('.icon-menu-open');
+const iconBurger = document.querySelector('.header-icon-open');
+const iconClose = document.querySelector('.header-icon-close');
 
-  if (currentIcon === './images/icons.svg#icon-align-left') {
-    menuIconUse.setAttribute('href', './images/icons.svg#icon-x-close');
-  } else {
-    menuIconUse.setAttribute('href', './images/icons.svg#icon-align-left');
-  }
+menuOpen.addEventListener('click', onClick);
+
+function onClick(e) {
+  iconBurger.classList.toggle('icon-display-none');
+  iconClose.classList.toggle('icon-display-none');
 }
