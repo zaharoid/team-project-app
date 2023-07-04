3e97cd2d79ed53f586dab1ce78b30c65bb93ba91
zaharoid <gololobov.zahar23@gmail.com>
2023-07-04 20:54:41 +0200

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,31 +1,31 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set } from 'firebase/database';
+// import { initializeApp } from 'firebase/app';
+// import { getDatabase, ref, set } from 'firebase/database';
 
-const API_KEY = 'AIzaSyDxNwmZzHZ-vdGILRkmWY0qu02lzG2Ospc';
+// const API_KEY = 'AIzaSyDxNwmZzHZ-vdGILRkmWY0qu02lzG2Ospc';
 
-firebaseConfig = {
-  databaseURL: `gs://books-project-c0eb5.appspot.com`,
-};
+// firebaseConfig = {
+//   databaseURL: `gs://books-project-c0eb5.appspot.com`,
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-// const database = getDatabase(app);
+// // const database = getDatabase(app);
 
-function writeUserData(userId, name, email) {
-  const db = getDatabase(app);
-  console.log(db);
+// function writeUserData(userId, name, email) {
+//   const db = getDatabase(app);
+//   console.log(db);
 
-  set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email,
-  });
-}
+//   set(ref(db, 'users/' + userId), {
+//     username: name,
+//     email: email,
+//   });
+// }
 
-writeUserData(
-  'DKt5yaLfQNTI9LVZtnLPhYqLJk63',
-  'ilya',
-  'tkachenkoilya07@icloud.com'
-);
+// writeUserData(
+//   'DKt5yaLfQNTI9LVZtnLPhYqLJk63',
+//   'ilya',
+//   'tkachenkoilya07@icloud.com'
+// );
 
 const openBtn = document.querySelector('.user-btn');
 openBtn.addEventListener('click', openModal);
