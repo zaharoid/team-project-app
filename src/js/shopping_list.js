3efe9bf11094bbe6da7127061d00113cc13c4068
alexroi80 <alexroi80@gmail.com>
2023-07-05 12:11:32 +0300

    shopList changed

--- src/js/shopping_list.js
+++ src/js/shopping_list.js
@@ -4,17 +4,24 @@ import './modal-window/modal';
 import './header';
 import './auth';
 
-const selectedBookList = document.querySelector('.shopping-list');
-const removeBookBtn = document.querySelector('.remove-book');
-const selectedBooks = JSON.parse(localStorage.getItem('selected')) ?? [];
-console.log(selectedBooks.length>0);
+import noBooksImage from "../images/shopping-list/list-placeholder.webp";
+import bookImagePlaceholder from "../images/shopping-list/book-image-placeholder.webp";
+import svgIcons from "../images/icons.svg";
 
-createMarkup(selectedBooks, selectedBookList);
-
-if(selectedBooks.length>0){
-  removeBookBtn.addEventListener('click', removeBook);
+const refs={
+  selectedBookList:document.querySelector('.shopping-list'),
+  removeBookBtn:document.querySelector('.remove-book'),
+  selectedBooks:JSON.parse(localStorage.getItem('selected')) ?? [],
 }
 
+// const selectedBookList = document.querySelector('.shopping-list');
+// const removeBookBtn = document.querySelector('.remove-book');
+// const selectedBooks = JSON.parse(localStorage.getItem('selected')) ?? [];
+createMarkup(refs.selectedBooks, refs.selectedBookList);
+
+if(refs.selectedBooks.length>0){
+  removeBookBtn.addEventListener('click', removeBook);
+}
 
 function createMarkup(arr, selectedBookList) {
   let markup;
@@ -30,7 +37,7 @@ function createMarkup(arr, selectedBookList) {
           description,
           buy_links,
         }) => ` <li class="book-card data-id="${_id}" >
-          <div class="book-image-wrapper"><img src="${book_image}" alt="${title}" class="book-image"></div>
+          <div class="book-image-wrapper"><img src="${book_image}" alt="${title}" class="book-image" onerror="this.src=${bookImagePlaceholder};"></div>
           <div class="book-info">
               <h2 class="book-title">${title}</h2>
               <p class="book-category">${list_name}</p>
@@ -53,7 +60,7 @@ function createMarkup(arr, selectedBookList) {
   } else {
     markup = `<li class="empty-list-card">
       <p class="empty-list-text">This page is empty, add some books and proceed to order.</p>
-      <img src="./images/shopping-list/list-placeholder.webp" alt="No books added" class="empty-list-image">
+      <img src=${noBooksImage} alt="No books added" class="empty-list-image">
     </li>`;
   }
   selectedBookList.innerHTML = markup;
@@ -79,7 +86,7 @@ function createBuyLinks(arr) {
         return `<li class="buy-link-item">
       <a href="${data.url}" target="_blank">
       <svg class="buy-link apple">
-      <use href="/src/images/icons.svg#icon-apple"></use>
+      <use href="${svgIcons}#icon-apple"></use>
       </svg></a>
       </li>`;
       }
@@ -98,5 +105,5 @@ function createBuyLinks(arr) {
 
 function findBook(elem) {
   const bookId = elem.closest('.book-card').dataset.id;
-  return selectedBooks.find(({ id }) => id === bookId);
+  return refs.selectedBooks.find(({ id }) => id === bookId);
 }
