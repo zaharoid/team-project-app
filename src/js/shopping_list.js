6f220ef2215c2b894578cccc1ada315a19df3a85
alexroi80 <alexroi80@gmail.com>
2023-07-06 01:30:58 +0300

    shopping

--- src/js/shopping_list.js
+++ src/js/shopping_list.js
@@ -1,29 +1,25 @@
-
 import './home__support';
 import './dark-theme';
-import './modal-window/modal';
+// import './modal-window/modal';
 import './header';
 import './auth';
 
-import noBooksImage from "../images/shopping-list/list-placeholder.webp";
-import bookImagePlaceholder from "../images/shopping-list/book-image-placeholder.webp";
-import svgIcons from "../images/icons.svg";
+import noBooksImage from '../images/shopping-list/list-placeholder.webp';
+import bookImagePlaceholder from '../images/shopping-list/book-image-placeholder.webp';
+import svgIcons from '../images/icons.svg';
+import bookShopIcon from '../images/bookshop.png';
 
-const refs={
-  selectedBookList:document.querySelector('.shopping-list'),
-  removeBookBtn:document.querySelector('.remove-book'),
-  selectedBooks:JSON.parse(localStorage.getItem('selected')) ?? [],
-}
+const refs = {
+  selectedBookList: document.querySelector('.shopping-list'),
+  selectedBooks: JSON.parse(localStorage.getItem('booksToBuy')) ?? [],
+};
 
-// const selectedBookList = document.querySelector('.shopping-list');
-// const removeBookBtn = document.querySelector('.remove-book');
-// const selectedBooks = JSON.parse(localStorage.getItem('selected')) ?? [];
 createMarkup(refs.selectedBooks, refs.selectedBookList);
 
-if(refs.selectedBooks.length>0){
+if (refs.selectedBooks.length > 0) {
+  const removeBookBtn = document.querySelector('.remove-book');
   removeBookBtn.addEventListener('click', removeBook);
 }
-
 
 function createMarkup(arr, selectedBookList) {
   let markup;
@@ -31,53 +27,54 @@ function createMarkup(arr, selectedBookList) {
     markup = arr
       .map(
         ({
-          _id,
+          id,
           author,
-          list_name,
+          category,
           title,
-          book_image,
+          image,
           description,
-          buy_links,
-        }) => ` <li class="book-card data-id="${_id}" >
-
-          <div class="book-image-wrapper"><img src="${book_image}" alt="${title}" class="book-image" onerror="this.src=${bookImagePlaceholder};"></div>
-
-
-
+          links,
+        }) => ` <li class="book-card" data-id="${id}" >
+          <div class="book-image-wrapper">
+          <img src="${image}" alt="${title}" class="book-image" onerror="this.src=${bookImagePlaceholder}"></div>
           <div class="book-info">
               <h2 class="book-title">${title}</h2>
-              <p class="book-category">${list_name}</p>
+              <p class="book-category">${category}</p>
               <p class="book-descr">${description}</p>
               <div class="card-inner-wrapper">
                   <h3 class="book-author">${author}</h3>
-
-
-                  <ul class="buy-links-list">${createBuyLinks(buy_links)}</ul>  
-
                   <div class="buy-links-wrapper">
-                  <svg class="buy-link">
-      <use href="./images/icons.svg#icon-trash"></use>
-    </svg>
-    <svg class="buy-link">
-      <use href="./images/icons.svg#icon-trash"></use>
-    </svg>
-    <svg class="buy-link">
-      <use href="./images/icons.svg#icon-trash"></use>
-    </svg>
+                  <ul class="buy-links-list">
+                  <li class="buy-link-item">
+                  <a href=${links[0]} target="_blank">
+                  <svg class="buy-link amazon">
+                  <use href="${svgIcons}#icon-amazon"></use>
+                  </svg></a>
+                  </li>
+                  <li class="buy-link-item">
+                  <a href="${links[1]}" target="_blank">
+                  <svg class="buy-link apple">
+                  <use href="${svgIcons}#icon-apple"></use>
+                  </svg></a>
+                  </li>
+                  <li class="buy-link-item">
+                  <a href="${links[2]}" target="_blank">
+                  <svg class="buy-link bookshop">
+                  <use href="${bookShopIcon}"></use>
+                  </svg></a>
+                  </li>
+                  </ul> 
                   </div>
-
-
-              </div>
+                  </div>
               <button type="button" class="remove-book">
               <svg class="remove-image">
-      <use href="./images/icons.svg#icon-trash"></use>
+      <use href="${svgIcons}#icon-trash"></use>
     </svg>
               </button>
           </div>
           
           
       </li>`
-
       )
       .join('');
   } else {
@@ -87,49 +84,18 @@ function createMarkup(arr, selectedBookList) {
 
     </li>`;
   }
-  selectedBookList.innerHTML = markup;
+ return refs.selectedBookList.innerHTML = markup;
 }
 
 function removeBook(evt) {
   evt.preventDefault();
-}
-
-function createBuyLinks(arr) {
-  return arr
-    .map(data => {
-      if (data.name === 'Amazon') {
-        return `<li class="buy-link-item">
-      <a href=${data.url} target="_blank">
-      <svg class="buy-link amazon">
-      <use href=/src/images/icons.svg#icon-amazon></use>
-      </svg></a>
-      </li>
-      `;
-      }
-      if (data.name === 'Apple Books') {
-        return `<li class="buy-link-item">
-      <a href="${data.url}" target="_blank">
-      <svg class="buy-link apple">
-      <use href="${svgIcons}#icon-apple"></use>
-
-      </svg></a>
-      </li>`;
-      }
-      if (data.name === 'Bookshop') {
-        return `<li class="buy-link-item">
-    <a href="${data.url}" target="_blank">
-    <svg class="buy-link bookshop">
-    <use href="/src/images/icons.svg#bookshop"></use>
-    </svg></a>
-    </li>
-    `;
-      }
-    })
-    .join('');
+  let bookIndex = findBook(evt.target);
+  refs.selectedBooks.splice(bookIndex, 1);
+  localStorage.setItem('booksToBuy', JSON.stringify(refs.selectedBooks));
+  return createMarkup(refs.selectedBooks, refs.selectedBookList); 
 }
 
 function findBook(elem) {
-  const bookId = elem.closest('.book-card').dataset.id;
-  return refs.selectedBooks.find(({ id }) => id === bookId);
+  let bookId = elem.closest('.book-card').dataset.id;
+  return refs.selectedBooks.findIndex(book =>book.id === bookId);
 }
-
