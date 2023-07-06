e5dc8eca3925ae8e900c811636bba80f1f443bf3
zaharoid <gololobov.zahar23@gmail.com>
2023-07-06 17:51:58 +0200

    .

--- src/js/modal-window/firebase.js
+++ src/js/modal-window/firebase.js
@@ -1,125 +1,125 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  connectAuthEmulator,
-  signInWithEmailAndPassword,
-  AuthErrorCodes,
-  onAuthStateChanged,
-  signOut,
-} from 'firebase/auth';
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import {
+//   getAuth,
+//   createUserWithEmailAndPassword,
+//   connectAuthEmulator,
+//   signInWithEmailAndPassword,
+//   AuthErrorCodes,
+//   onAuthStateChanged,
+//   signOut,
+// } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBJxM0QaklTSE-TkJnEoE7MpdsduKlooE0',
-  authDomain: 'bookshop-500.firebaseapp.com',
-  projectId: 'bookshop-500',
-  storageBucket: 'bookshop-500.appspot.com',
-  messagingSenderId: '101931220235',
-  appId: '1:101931220235:web:b46782386513f87faa360e',
-  measurementId: 'G-G1XWB30RZX',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBJxM0QaklTSE-TkJnEoE7MpdsduKlooE0',
+//   authDomain: 'bookshop-500.firebaseapp.com',
+//   projectId: 'bookshop-500',
+//   storageBucket: 'bookshop-500.appspot.com',
+//   messagingSenderId: '101931220235',
+//   appId: '1:101931220235:web:b46782386513f87faa360e',
+//   measurementId: 'G-G1XWB30RZX',
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-// createUserWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // ..
+// const auth = getAuth(app);
+// // createUserWithEmailAndPassword(auth, email, password)
+// //   .then((userCredential) => {
+// //     // Signed in
+// //     const user = userCredential.user;
+// //     // ...
+// //   })
+// //   .catch((error) => {
+// //     const errorCode = error.code;
+// //     const errorMessage = error.message;
+// //     // ..
+// //   });
+
+// connectAuthEmulator(auth, 'http://localhost:9099');
+
+// const loginEmailPass = async e => {
+//   e.preventDefault();
+//   const emailVal = login.value;
+//   const passVal = pass.value;
+
+//   try {
+//     const userCredential = await signInWithEmailAndPassword(
+//       auth,
+//       emailVal,
+//       passVal
+//     );
+//   } catch (error) {
+//     console.log('error', error);
+//     showLoginError(error);
+//   }
+// };
+
+// const divLoginError = document.querySelector('.login_error');
+// const btnLogin = document.querySelector('.btn_login');
+// const pass = document.querySelector("[name='password']");
+// const login = document.querySelector("[name='email']");
+// const ligin_name = document.querySelector("[name='name']");
+// const loginMessageError = document.querySelector('.login_error');
+// const logOut = document.querySelector('.btn_logout');
+
+// btnLogin.addEventListener('click', loginEmailPass);
+
+// const hideLoginError = () => {
+//   divLoginError.style.display = 'none';
+//   loginMessageError.innerHTML = '';
+// };
+
+// const showLoginError = error => {
+//   divLoginError.style.display = 'block';
+//   if (error.code === AuthErrorCodes.INVALID_PASSWORD) {
+//     loginMessageError.innerHTML = 'wrong password';
+//   } else {
+//     loginMessageError.innerHTML = `Error: ${error.message}`;
+//   }
+// };
+
+// const showLoginState = user => {
+//   // lblAuthState.innerHTML = `You're logged in as ${user.displayName} ( uid: ${user.uid}, email: ${user.email})`
+// };
+
+// hideLoginError();
+
+// const createAccount = async e => {
+//   const emailValue = login.value;
+//   const passValue = pass.value;
+
+//   try {
+//     e.preventDefault();
+//     const userCredential = await createUserWithEmailAndPassword(
+//       auth,
+//       emailValue,
+//       passValue
+//     );
+//     console.log('first', userCredential.user);
+//   } catch (error) {
+//     console.log('error', error);
+//     showLoginError(error);
+//   }
+// };
+
+// btnLogin.addEventListener('click', createAccount);
+
+// const monitorAuthState = async () => {
+//   onAuthStateChanged(auth, user => {
+//     if (user) {
+//       console.log('user', user);
+//       // showApp()
+//       showLoginState(user);
+//       hideLoginError();
+//     } else {
+//       showLoginForm();
+//       lblAuthState.innerHTML = `You're not logged in`;
+//     }
 //   });
+// };
 
-connectAuthEmulator(auth, 'http://localhost:9099');
-
-const loginEmailPass = async e => {
-  e.preventDefault();
-  const emailVal = login.value;
-  const passVal = pass.value;
-
-  try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      emailVal,
-      passVal
-    );
-  } catch (error) {
-    console.log('error', error);
-    showLoginError(error);
-  }
-};
-
-const divLoginError = document.querySelector('.login_error');
-const btnLogin = document.querySelector('.btn_login');
-const pass = document.querySelector("[name='password']");
-const login = document.querySelector("[name='email']");
-const ligin_name = document.querySelector("[name='name']");
-const loginMessageError = document.querySelector('.login_error');
-const logOut = document.querySelector('.btn_logout');
-
-btnLogin.addEventListener('click', loginEmailPass);
-
-const hideLoginError = () => {
-  divLoginError.style.display = 'none';
-  loginMessageError.innerHTML = '';
-};
-
-const showLoginError = error => {
-  divLoginError.style.display = 'block';
-  if (error.code === AuthErrorCodes.INVALID_PASSWORD) {
-    loginMessageError.innerHTML = 'wrong password';
-  } else {
-    loginMessageError.innerHTML = `Error: ${error.message}`;
-  }
-};
-
-const showLoginState = user => {
-  // lblAuthState.innerHTML = `You're logged in as ${user.displayName} ( uid: ${user.uid}, email: ${user.email})`
-};
-
-hideLoginError();
-
-const createAccount = async e => {
-  const emailValue = login.value;
-  const passValue = pass.value;
-
-  try {
-    e.preventDefault();
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      emailValue,
-      passValue
-    );
-    console.log('first', userCredential.user);
-  } catch (error) {
-    console.log('error', error);
-    showLoginError(error);
-  }
-};
-
-btnLogin.addEventListener('click', createAccount);
-
-const monitorAuthState = async () => {
-  onAuthStateChanged(auth, user => {
-    if (user) {
-      console.log('user', user);
-      // showApp()
-      showLoginState(user);
-      hideLoginError();
-    } else {
-      showLoginForm();
-      lblAuthState.innerHTML = `You're not logged in`;
-    }
-  });
-};
-
-// monitorAuthState()
-// const logout = async () => {
-//     await signOut(auth)
-// }
-// logOut.addEventListener('click', logout)
+// // monitorAuthState()
+// // const logout = async () => {
+// //     await signOut(auth)
+// // }
+// // logOut.addEventListener('click', logout)
--- src/js/modal-window/modal_log.js
+++ src/js/modal-window/modal_log.js
@@ -1,9 +1,74 @@
+// import { blockScroll } from './modal';
+
+// const sing_up_btn = document.querySelector('.btn_sign_up');
+// const sing_in_btn = document.querySelector('.btn_sign_in');
+// const user_log_in_btn = document.querySelector('.register-btn');
+// const login_btn_close = document.querySelector('modal-svg');
+
+// let modal_action = document.querySelector('.login_action');
+// let modalLoginBody = document.querySelector('body');
+
+// sing_up_btn.addEventListener('click', preventDefault);
+// sing_in_btn.addEventListener('click', preventDefault);
+// user_log_in_btn.addEventListener('click', createLoginModalMarkup);
+// console.log(user_log_in_btn);
+
+// function preventDefault(e) {
+//   e.preventDefault();
+// }
+
+// modalLoginBody.style.paddingRight = '0';
+
+// function createLoginModalMarkup(e) {
+//   modalLoginBody.style.paddingRight = '18px';
+//   blockScroll();
+//   modalLoginBody.classList.add('login_overlay');
+//   modal_action.style.visibility = 'visible';
+//   modal_action.style.opacity = '1';
+//   eventLoginListeners();
+// }
+
+// function eventLoginListeners() {
+//   const buttonClose = document.querySelector('.modal_login_btn_close');
+//   buttonClose.addEventListener('click', closeLoginModal);
+//   window.addEventListener('keydown', onEscCloseModal);
+//   modalLoginBody.addEventListener('click', onOverlayCloseModal);
+//   modalLoginBody.addEventListener('scroll', e => {
+//     e.preventDefault();
+//   });
+// }
+
+// function onOverlayCloseModal(e) {
+//   if (e.target !== modal_action) {
+//     return;
+//   }
+//   closeLoginModal();
+// }
+
+// function onEscCloseModal(e) {
+//   if (e.code !== 'Escape') {
+//     return;
+//   }
+//   closeLoginModal();
+// }
+
+// function closeLoginModal() {
+//   modalLoginBody.classList.remove('no_scroll');
+//   modalLoginBody.style.paddingRight = '0';
+//   modalLoginBody.classList.remove('login_overlay');
+//   modal_action.style.visibility = 'hidden';
+//   modal_action.style.opacity = '0';
+//   modalLoginBody.removeEventListener('click', onOverlayCloseModal);
+//   window.removeEventListener('keydown', onEscCloseModal);
+// }
+
 import { blockScroll } from './modal';
 
 const sing_up_btn = document.querySelector('.btn_sign_up');
 const sing_in_btn = document.querySelector('.btn_sign_in');
 const user_log_in_btn = document.querySelector('.register-btn');
 const login_btn_close = document.querySelector('modal-svg');
+const modalLoginAction = document.querySelector('.overlay_log');
 
 let modal_action = document.querySelector('.login_action');
 let modalLoginBody = document.querySelector('body');
@@ -11,7 +76,6 @@ let modalLoginBody = document.querySelector('body');
 sing_up_btn.addEventListener('click', preventDefault);
 sing_in_btn.addEventListener('click', preventDefault);
 user_log_in_btn.addEventListener('click', createLoginModalMarkup);
-console.log(user_log_in_btn);
 
 function preventDefault(e) {
   e.preventDefault();
@@ -23,8 +87,8 @@ function createLoginModalMarkup(e) {
   modalLoginBody.style.paddingRight = '18px';
   blockScroll();
   modalLoginBody.classList.add('login_overlay');
-  modal_action.style.visibility = 'visible';
-  modal_action.style.opacity = '1';
+  modalLoginAction.style.visibility = 'visible';
+  modalLoginAction.style.opacity = '1';
   eventLoginListeners();
 }
 
@@ -39,7 +103,7 @@ function eventLoginListeners() {
 }
 
 function onOverlayCloseModal(e) {
-  if (e.target !== modal_action) {
+  if (e.target !== modalLoginAction) {
     return;
   }
   closeLoginModal();
@@ -56,8 +120,8 @@ function closeLoginModal() {
   modalLoginBody.classList.remove('no_scroll');
   modalLoginBody.style.paddingRight = '0';
   modalLoginBody.classList.remove('login_overlay');
-  modal_action.style.visibility = 'hidden';
-  modal_action.style.opacity = '0';
+  modalLoginAction.style.visibility = 'hidden';
+  modalLoginAction.style.opacity = '0';
   modalLoginBody.removeEventListener('click', onOverlayCloseModal);
   window.removeEventListener('keydown', onEscCloseModal);
 }
