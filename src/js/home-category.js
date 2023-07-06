b20a6ff33aa8c10243ba006da536ba1b103b107d
vldslvD <dobritsavlad@gmail.com>
2023-07-06 04:43:17 +0200

    WE NEED MORE FIXES!!!!

--- src/js/home-category.js
+++ src/js/home-category.js
@@ -1,31 +1,35 @@
 import { lastWordChange } from './books-container';
-import { renderAllCards} from './books-container';
-import { fetchTopBooks, fetchBookCategories, fetchBookByCategory } from './books-api';
+import { renderAllCards } from './books-container';
+import {
+  fetchTopBooks,
+  fetchBookCategories,
+  fetchBookByCategory,
+} from './books-api';
 const refs = {
-  loaderEl: document.querySelector('.loader '),
+  loaderEl: document.querySelector('.loader'),
   divEl: document.querySelector('.category-list'),
   allcategory: document.querySelector('.all-category'),
   onecategoryEl: document.querySelector('.category-link'),
   categoriesList: document.querySelector('.books-in-categories-list'),
-   loader: document.querySelector('#loader')
+  loader: document.querySelector('#loader-booklist'),
 };
 
 refs.divEl.classList.add('is-hidden');
-refs.categoriesList.classList.add('is-hidden')
+refs.categoriesList.classList.add('is-hidden');
 
-fetchBookCategories().then(data => { 
-   const firstCategoryMarkup = `<li class="list category-item">
+fetchBookCategories().then(data => {
+  const firstCategoryMarkup = `<li class="list category-li">
             <a class="link category-link is-active" href="#" id="all">All categories</a>
-        </li>`
+        </li>`;
   refs.divEl.insertAdjacentHTML('beforeend', firstCategoryMarkup);
   data.map(book => {
-    const markup = `<li class="list category-item">
+    const markup = `<li class="list category-li">
         <a class="link category-link" href="#">${book.list_name}</a>
       </li>`;
     refs.divEl.insertAdjacentHTML('beforeend', markup);
-    
-    refs.divEl.classList.remove('is-hidden')
-     refs.loaderEl.classList.add('is-hidden')
+
+    refs.divEl.classList.remove('is-hidden');
+    refs.loaderEl.classList.add('is-hidden');
   });
 });
 // refs.allcategory.focus()
@@ -41,31 +45,26 @@ function onCategoryClick(e) {
   }
   e.target.classList.add('is-active');
   let titleCategory = document.querySelector('.booklist-title');
-  
-  if (e.target.id === "") {
+
+  toggleBooksLoader();
+
+  if (e.target.id === '') {
     let titleTextContent = e.target.textContent;
     titleCategory.innerHTML = lastWordChange(titleTextContent);
     refs.categoriesList.classList.add('render-category');
     fetchBookByCategory(e.target.textContent).then(data =>
-      renderMarkupBooksByCategory(takeBookMarkupByCategory(data)
-      )
-      
+      renderMarkupBooksByCategory(takeBookMarkupByCategory(data))
     );
-  }
-  else {
-      refs.categoriesList.classList.toggle('is-hidden')
-  refs.loader.classList.toggle('is-hidden')
-    
-    refs.categoriesList.innerHTML = "";
-    titleCategory.innerHTML = lastWordChange("Best Sellers Books");
+
+  } else {
+    refs.categoriesList.innerHTML = '';
+    titleCategory.innerHTML = lastWordChange('Best Sellers Books');
     refs.categoriesList.classList.add('render-category');
     fetchTopBooks().then(renderAllCards);
-    
   }
-
 }
 function takeBookMarkupByCategory(books) {
-
+  toggleBooksLoader();
   let markup = books
     .map(({ author, title, book_image, _id }) => {
       return `
@@ -83,9 +82,7 @@ function takeBookMarkupByCategory(books) {
   return markup;
 }
 function renderMarkupBooksByCategory(markup) {
-  refs.categoriesList.innerHTML = markup;  
-  
- 
+  refs.categoriesList.innerHTML = markup;
 }
 
 refs.categoriesList.addEventListener('click', onMoreBtnClick);
@@ -94,20 +91,27 @@ function onMoreBtnClick(e) {
   if (e.target.nodeName !== 'BUTTON') {
     return;
   }
-  let activeBook = document.querySelector('.is-active')
-    activeBook.classList.remove('is-active');
+  let activeBook = document.querySelector('.is-active');
+  activeBook.classList.remove('is-active');
 
-  const links = document.querySelectorAll(".category-link");
+  const links = document.querySelectorAll('.category-link');
   links.forEach(link => {
     if (link.textContent === e.target.getAttribute('data-category-name')) {
       link.classList.add('is-active');
       link.scrollIntoView();
     }
   });
-    let titleCategory = document.querySelector('.booklist-title');
-    titleCategory.innerHTML = lastWordChange(e.target.getAttribute('data-category-name'));
-    refs.categoriesList.classList.add('render-category');
-    fetchBookByCategory(e.target.getAttribute('data-category-name')).then(data =>
-      renderMarkupBooksByCategory(takeBookMarkupByCategory(data))
-    );
+  let titleCategory = document.querySelector('.booklist-title');
+  titleCategory.innerHTML = lastWordChange(
+    e.target.getAttribute('data-category-name')
+  );
+  refs.categoriesList.classList.add('render-category');
+  fetchBookByCategory(e.target.getAttribute('data-category-name')).then(data =>
+    renderMarkupBooksByCategory(takeBookMarkupByCategory(data))
+  );
+}
+
+function toggleBooksLoader() {
+  refs.categoriesList.classList.toggle('is-hidden');
+  refs.loader.classList.toggle('is-hidden');
 }
