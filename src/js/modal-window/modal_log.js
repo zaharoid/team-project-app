3bea512b23b8296a2f8c90c63d931eee12054d9c
KRVitalik <karmelitavitalik@gmail.com>
2023-07-06 17:08:36 +0300

    comit

--- src/js/modal-window/modal_log.js
+++ src/js/modal-window/modal_log.js
@@ -2,50 +2,47 @@ import {
     blockScroll
 } from "./modal"
 
-
-const user_log_in_btn = document.querySelector(".button-container") 
+const sing_up_btn = document.querySelector('.btn_sign_up')
+const sing_in_btn = document.querySelector('.btn_sign_in')
+const user_log_in_btn = document.querySelector(".register-btn") 
 const login_btn_close = document.querySelector("modal-svg")
 
 let modal_action = document.querySelector('.login_action');
 let modalLoginBody = document.querySelector('body');
+// modalLoginBody.style.paddingRight = '0px';
 
-
+sing_up_btn.addEventListener('click', preventDefault)
+sing_in_btn.addEventListener('click', preventDefault)
 user_log_in_btn.addEventListener('click', createLoginModalMarkup)
 console.log(user_log_in_btn)
 
+function preventDefault(e) {
+    e.preventDefault();
+}
 
 function createLoginModalMarkup(e) {
     blockScroll();
-    eventLoginListeners();
     modalLoginBody.classList.add('login_overlay');
-    modalLoginBody.style.paddingRight = '18px';
     modal_action.style.visibility = 'visible';
+    // modalLoginBody.style.paddingRight = '18px';
     modal_action.style.opacity = '1';
+    eventLoginListeners();
 }
 
 function eventLoginListeners() {
   const buttonClose = document.querySelector('.modal_login_btn_close');
   buttonClose.addEventListener('click', closeLoginModal);
-  modalLoginBody.addEventListener('click', onOverlayCloseModal);
-  window.addEventListener('keydown', onEscCloseModal);
+    window.addEventListener('keydown', onEscCloseModal);
+      modalLoginBody.addEventListener('click', onOverlayCloseModal);
   modalLoginBody.addEventListener('scroll', e => {
     e.preventDefault();
   });
 }
 
-function closeLoginModal() {
-  modalLoginBody.classList.remove('no_scroll');
-  modalLoginBody.style.paddingRight = '0px';
-  modalLoginBody.classList.remove('login_overlay');
-  modal_action.style.visibility = 'hidden';
-  modal_action.style.opacity = '0';
-  modalLoginBody.removeEventListener('click', onOverlayCloseModal);
-  window.removeEventListener('keydown', onEscCloseModal);
-}
 function onOverlayCloseModal(e) {
-  if (e.target !== modal_action) {
-    return;
-  }
+    if (e.target !== modal_action) {
+        return;
+    }
   closeLoginModal();
 }
 
@@ -54,4 +51,14 @@ function onEscCloseModal(e) {
     return;
   }
   closeLoginModal();
+}
+
+function closeLoginModal() {
+  modalLoginBody.classList.remove('no_scroll');
+  modalLoginBody.style.paddingRight = '0';
+  modalLoginBody.classList.remove('login_overlay');
+  modal_action.style.visibility = 'hidden';
+  modal_action.style.opacity = '0';
+  modalLoginBody.removeEventListener('click', onOverlayCloseModal);
+  window.removeEventListener('keydown', onEscCloseModal);
 }
